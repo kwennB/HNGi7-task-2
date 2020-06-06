654f531ae17d04a15a20086f0fe0dc796183df77
KwennB <60541622+kwennB@users.noreply.github.com>
2020-06-06 22:32:16 +0100

    Update and rename KwennB-HNG-01909.js to Kwennb-HNG-01909.js

---
+++ scripts/Kwennb-HNG-01909.js
@@ -0,0 +1,10 @@
+const fullName = "Anyebe Blessing";
+const id = "HNG-01909";
+const emailAddr = "anyebeblessing737@yahoo.com";
+const language = "JavaScript";
+
+const personalInfo = ()=>{
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${emailAddr} using ${language} for stage 2 task`)
+};
+
+personalInfo();
